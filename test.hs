5518668a6afca386fbbb598d0567e748da69c821
Dani <dani_rl94@hotmail.com>
2016-04-01 17:44:51 +0200

    fixed bugs

--- test.hs
+++ test.hs
@@ -1,5 +1,5 @@
 data Command a = Assign String (NumExpr a) | Input String | Print String| Seq [Command a]
-    | Cond (BoolExpr a) (Command a) | Loop [BoolExpr a] (Command a) deriving(Show)
+    | Cond [BoolExpr a] (Command a) | Loop [BoolExpr a] (Command a) deriving(Show)
 
 data NumExpr a = Var String | Const a | Plus (NumExpr a) (NumExpr a) | Minus (NumExpr a) (NumExpr a)
     | Times (NumExpr a) (NumExpr a) | Div (NumExpr a) (NumExpr a) deriving(Show)
@@ -9,10 +9,11 @@ data BoolExpr a = AND (BoolExpr a) (BoolExpr a) | OR (BoolExpr a) (BoolExpr a) |
 
 readCommand:: (Read a,Num a) => String -> Command a
 readCommand entrada
-    | (head tokens) == "INPUT" = Input $ last tokens
-    | (head tokens) == "PRINT" = Print $ last tokens
+    | (head tokens) == "INPUT" = Input $ tokens!!1
+    | (head tokens) == "PRINT" = Print $ tokens!!1
     | (tokens!!1) == ":=" = Assign (head tokens) (readNumExpr (drop 2 tokens))
     | (head tokens) == "WHILE" = Loop (readBoolExpr (tail tokens)) (readCommand (unwords(drop 1(dropWhile (/="DO") tokens)))) 
+    | (head tokens) == "IF" = Cond (readBoolExpr (tail tokens)) (readCommand (unwords(drop 1(dropWhile (/="THEN") tokens)))) 
     | (head tokens) == "DO" = Input (unwords ["Hola"])
         where
             tokens = words entrada
@@ -22,6 +23,14 @@ readCommand entrada
 --X := X - 1;
 --Z := Z * Z
 --END
+readCommandList:: (Read a,Num a) => String -> [Command a]
+readCommandList entrada
+    | tokens == [] = []
+    | otherwise = [readCommand entrada]++(readCommandList (unwords(drop 1(dropWhile (checkValue) tokens))))
+        where
+            tokens = words entrada
+            checkValue a = ((a /= "THEN") && (a /= "END") && ((last a) /= ';'))
+
 
 readNumExpr::(Read a,Num a) => [String] -> NumExpr a
 readNumExpr tokens
@@ -34,7 +43,7 @@ readBoolExpr::(Read a,Num a) => [String] -> [BoolExpr a]
 readBoolExpr tokens
     | ((head tokens) == "END") || ((head tokens) == "THEN") || ((head tokens) == "DO")= []
     | (head tokens) == "NOT" = [NOT (head $ readBoolExpr (tail tokens))]++(readBoolExpr (tail tokens))
-    | (tokens!!1) == "Gt" = [Gt (readNumExpr [(head tokens)]) (readNumExpr [(tokens!!2)])]++(readBoolExpr (drop 3 tokens))
+    | (tokens!!1) == ">" = [Gt (readNumExpr [(head tokens)]) (readNumExpr [(tokens!!2)])]++(readBoolExpr (drop 3 tokens))
 
 
 
